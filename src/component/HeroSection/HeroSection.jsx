4cd8439e63c76ebff9067204c2aba2b76ae7b749
Megha <bathlamegha6@gmail.com>
2025-03-23 15:14:19 +0530

    move images from assets to public folder

--- src/component/HeroSection/HeroSection.jsx
+++ src/component/HeroSection/HeroSection.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./HeroSection.css";
 import { Button } from "../Button/Button";
 
--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,13 +1,17 @@
 import "./Navbar.css";
 import { IoSearch } from "react-icons/io5";
-import Slack_logo_2 from "../../assets/Slack_logo_2.png";
+// import Slack_logo_2 from "../../assets/Slack_logo_2.png";
 import { Button } from "../Button/Button";
 
 export const Navbar = () => {
   return (
     <nav className="navbar_section">
       <div className="navbar_first_section">
-        <img className="logo" src={Slack_logo_2} alt="logo_slack" />
+        <img
+          className="logo"
+          src="/public/assets/Slack_logo_2.png"
+          alt="logo_slack"
+        />
 
         <ul className="nav_links">
           <li>Product</li>
